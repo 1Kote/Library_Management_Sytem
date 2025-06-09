6b151baf294792da9509ac8a481a6124f2adbc5c
1Kote <lioirq1@gmail.com>
2025-06-09 10:45:55 -0300

    feat: Foi instanciando um objeto da classe book para fins de testes e foi criada uma ArrayList do tipo Book para armazenar os objetos instanciados de book

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,11 +1,14 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalDate;
+import java.util.ArrayList;
 public class Main
 {
     public static void main(String[] args)
     {
+        Book book1 = new Book("Wind and Fire", "J. R. Martin", 530, "Fantasy", "3421234-1", LocalDate.of(2001, 10, 16), 10, 14);
 
-
-
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(book1);
     }
 }
