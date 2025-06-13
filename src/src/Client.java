45948594701f94dacd381ef86dd49b78d48c47f5
1Kote <lioirq1@gmail.com>
2025-06-13 09:11:37 -0300

    feat: Implementação do construtor na classe client e metodos getters e setters

--- src/src/Client.java
+++ src/src/Client.java
@@ -1,3 +1,5 @@
+import java.awt.color.ICC_ColorSpace;
+
 public class Client
 {
     private String cpf;
@@ -5,4 +7,58 @@ public class Client
     private String email;
     private String phoneNumber;
 
+    public Client(String cpf, String name, String email, String phoneNumber)
+    {
+        this.cpf = cpf;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    //getters
+    public String getCpf()
+    {
+        return cpf;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    //setters
+    public void setCpf(String cpf)
+    {
+        this.cpf = cpf;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+    public void setPhoneUmber(String phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String toString()
+    {
+        return  "\n---------Cliente---------"  +
+                "\nNome: " + this.getName() +
+                "\nCPF: " + this.getCpf() +
+                "\nEmail: " + this.getEmail() +
+                "\nphoneNumber" + this.getPhoneNumber() +
+                "\n-------------------------";
+
+    }
 }
